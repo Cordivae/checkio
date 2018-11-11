39b4d183d6718bbed96c6ac76083c32cd18031e9
Jacob Ramsey <jacob@techmonkey.pro>
2018-11-11 12:54:10 -0600

    Day's practice

---
+++ sun_angle.py
@@ -0,0 +1,14 @@
+def sun_angle(time):
+    time_num = int(time[0:2]) + int(time[-2:]) / 60
+    if time_num < 6 or time_num > 18:
+        return "I don't see the sun!"
+    return (time_num - 6) / 12 * 180
+
+if __name__ == '__main__':
+    print("Example:")
+    print(sun_angle("07:00"))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert sun_angle("07:00") == 15
+    assert sun_angle("01:23") == "I don't see the sun!"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
